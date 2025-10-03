ec240b99b01aed9841b5caf6aa8a1d104cd01173
Nikita Kibitkin <nikita.kibitkin.dev@proton.me>
2025-10-03 23:18:52 +0700

    cleaned

--- src/main/java/com/example/smoothing/smoothing/BackpressureGate.java
+++ src/main/java/com/example/smoothing/smoothing/BackpressureGate.java
@@ -9,16 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @Service
 public final class BackpressureGate {
     private final ThreadPoolExecutor executor;
     @Getter
-    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(20_000);
-    @Getter
-    private final AtomicLong credits;
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(2_000);
     private final Semaphore semaphore = new Semaphore(50);
     private final ScheduledExecutorService scheduler;
 
@@ -27,12 +24,11 @@ public final class BackpressureGate {
             @Value("${backpressure.credits}") long credits,
             @Qualifier("fastScheduler") ScheduledExecutorService scheduler
     ) {
-        this.credits = new AtomicLong(credits);
-        int maxWorkers = 200;
+        int maxWorkers = 50;
 
         this.executor = new ThreadPoolExecutor(
                 maxWorkers,
-                maxWorkers,
+                100,
                 0L, TimeUnit.MILLISECONDS,
                 new SynchronousQueue<>(),
                 runnable -> {
@@ -40,8 +36,7 @@ public final class BackpressureGate {
                     t.setDaemon(true);
                     return t;
                 },
-                // Когда все воркеры заняты — пусть вызывающий поток сам выполнит задачу
-                // Это даёт честный мгновенный backpressure и не накачивает очереди.
+                // Когда все воркеры заняты — пусть вызывающий поток сам выполнит задачу. Это даёт честный мгновенный backpressure и не накачивает очереди.
                 new ThreadPoolExecutor.CallerRunsPolicy()
         );
         this.scheduler = scheduler;
@@ -55,23 +50,12 @@ public final class BackpressureGate {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 log.error("queue interrupted", e);
-                return;
             }
         }
-//        if (semaphore.tryAcquire()){
-//            drain();
-//        }
     }
 
-//    public void grant(long n) {
-//        if (n > 0) {
-//            credits.addAndGet(n);
-//
-//            if (semaphore.tryAcquire()){
-//                drain();
-//            }
-//        }
-//    }
+
+
 
     @PostConstruct
     public void start() {
@@ -98,32 +82,6 @@ public final class BackpressureGate {
         }
     }
 
-
-//    private void drain() {
-//        try {
-//            while (true) {
-//                if (!tryAcquireCredit()) break;             // no credits → break
-//                Runnable r = queue.poll();                  // trying to take task
-//                if (r == null) {                            // no task → credit back
-//                    credits.incrementAndGet();
-//                    break;
-//                }
-//                executor.execute(r);                        // got a task → execute the task and spend credit
-//            }
-//        }catch (Exception e) {
-//            log.error("Error in drain()", e);
-//        }finally {
-//            semaphore.release();
-//        }
-//    }
-
-    private boolean tryAcquireCredit() {
-        while (true) {
-            long c = credits.get();
-            if (c <= 0) return false;
-            if (credits.compareAndSet(c, c - 1)) return true;
-        }
-    }
 
     @PreDestroy
     public void shutdown() {
